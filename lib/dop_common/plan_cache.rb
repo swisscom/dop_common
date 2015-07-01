743eebed93fd35623b4edb11851e676ff4302b71
Andreas Zuber <zuber@puzzle.ch>
2015-07-01 12:22:52 +0200

    Use the new plan name in the plan cache as identifier

--- lib/dop_common/plan_cache.rb
+++ lib/dop_common/plan_cache.rb
@@ -18,17 +18,16 @@ module DopCommon
 
     def add(plan_file)
       hash = YAML.load_file(plan_file)
-      id   = get_id(plan_file)
-      raise StandardError, 'Plan was already added. Remove to readd the plan' if id_exists?(id)
-
       plan = DopCommon::Plan.new(hash)
+
+      raise StandardError, 'Plan was already added. Remove to readd the plan' if plan_exists?(plan.name)
       raise StandardError, 'Plan not valid; did not add' unless plan.valid?
       raise StandardError, 'Some Nodes already exist. Did not add' if node_duplicates?(plan)
 
-      save_yaml(hash, id) # This may be removed once DOPv fully uses dop_common as parser
-      save(plan, id)
-      DopCommon.log.info("New plan #{plan_file} was added with id #{id}")
-      id
+      save_yaml(hash, plan.name) # This may be removed once DOPv fully uses dop_common as parser
+      save(plan)
+      DopCommon.log.info("New plan #{plan.name} was added")
+      plan.name
     end
 
     def list
@@ -37,41 +36,41 @@ module DopCommon
       end
     end
 
-    def remove(id)
-      raise StandardError, 'Plan id does not exist' unless id_exists?(id)
-      Dir[File.join(@plan_cache_dir, id + '*' )].each do |file|
+    def remove(name)
+      raise StandardError, "Plan #{name} does not exist" unless plan_exists?(name)
+      Dir[File.join(@plan_cache_dir, name + '*' )].each do |file|
         DopCommon.log.debug("Removing file from cache: #{file}")
         FileUtils.rm(file)
       end
-      DopCommon.log.info("Plan with id #{id} was removed")
-      id
+      DopCommon.log.info("Plan #{name} was removed")
+      name
     end
 
-    def id_exists?(id)
-      File.exists?(dump_file(id))
+    def plan_exists?(name)
+      File.exists?(dump_file(name))
     end
 
-    def get(id)
-      raise StandardError, 'Plan id does not exist' unless id_exists?(id)
-      YAML::load(File.read(dump_file(id)))
+    def get(name)
+      raise StandardError, "Plan #{name} does not exist" unless plan_exists?(name)
+      YAML::load(File.read(dump_file(name)))
     end
 
-    def save(plan, id)
-      File.open(dump_file(id), 'w') { |file| file.write(YAML::dump(plan)) }
+    def save(plan)
+      File.open(dump_file(plan.name), 'w') { |file| file.write(YAML::dump(plan)) }
     end
 
-    def save_yaml(hash, id)
-      File.open(yaml_file(id), 'w') { |file| file.write(hash) }
+    def save_yaml(hash, name)
+      File.open(yaml_file(name), 'w') { |file| file.write(hash) }
     end
 
     # Check if the nodes of the plan given as a parameter clash with
     # the nodes of the already stored planes in the cache
     def node_duplicates?(plan)
-      list.any? do |id|
-        get(id).nodes.any? do |cached_node|
+      list.any? do |name|
+        get(name).nodes.any? do |cached_node|
           plan.nodes.any? do |node|
             if node.name == cached_node.name
-              DopCommon.log.error("Node #{node.name} already exists in plan #{id}")
+              DopCommon.log.error("Node #{node.name} already exists in plan #{name}")
               true
             else
               false
@@ -81,17 +80,13 @@ module DopCommon
       end
     end
 
-    def yaml_file(id)
-      File.join(@plan_cache_dir, id + '.yaml')
+    def yaml_file(name)
+      File.join(@plan_cache_dir, name + '.yaml')
     end
 
     # Get the name of the dump file
-    def dump_file(id)
-      File.join(@plan_cache_dir, id + '_plan.yaml')
-    end
-
-    def get_id(plan_file)
-      Digest::SHA2.file(plan_file).hexdigest
+    def dump_file(name)
+      File.join(@plan_cache_dir, name + '_plan.yaml')
     end
 
   end
