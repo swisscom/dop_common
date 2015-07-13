489760ac52503d7b1180be7babaf02bcd281dd22
Andreas Zuber <zuber@puzzle.ch>
2015-07-13 11:54:23 +0200

    tests for nodes_by_config and exclude_nodes_by_config

--- spec/dop_common/step_spec.rb
+++ spec/dop_common/step_spec.rb
@@ -71,6 +71,35 @@ describe DopCommon::Step do
     end
   end
 
+  [:nodes_by_config, :exclude_nodes_by_config].each do |method_name|
+    describe '#' + method_name.to_s do
+      it 'returns a Hash if correctly specified' do
+        step = DopCommon::Step.new({:name => 'foo', method_name => {'my_alias' => 'foo'}})
+        expect(step.send(method_name)).to eq({'my_alias' => 'foo'})
+      end
+      it 'returns an empty hash if the key is missing' do
+        step = DopCommon::Step.new({:name => 'foo'})
+        expect(step.send(method_name)).to eq({})
+      end
+      it 'throws an exception if the value is something other than a hash' do
+        step = DopCommon::Step.new({:name => 'foo', method_name => 1})
+        expect{step.send(method_name)}.to raise_error DopCommon::PlanParsingError
+      end
+      it 'throws an exception if the hash contains something other than a string as a key' do
+        step = DopCommon::Step.new({:name => 'foo', method_name => {1 => 'foo'}})
+        expect{step.send(method_name)}.to raise_error DopCommon::PlanParsingError
+      end
+      it 'throws an exception if the hash contains something other than a string as a value' do
+        step = DopCommon::Step.new({:name => 'foo', method_name => {'my_alias' => 1}})
+        expect{step.send(method_name)}.to raise_error DopCommon::PlanParsingError
+      end
+      it 'throws an exception if the hash contains an invalid regexp' do
+        step = DopCommon::Step.new({:name => 'foo', method_name => {'my_alias' => '/][/'}})
+        expect{step.send(method_name)}.to raise_error DopCommon::PlanParsingError
+      end
+    end
+  end
+
   describe '#command' do
     it 'returns the command object if a command hash is specified' do
       step = DopCommon::Step.new({:name => 'foo', :command => {:plugin => 'dummy'}})
