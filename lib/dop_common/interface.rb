263346cfcdf37cf38e194fb02d3dd2808db7d49f
Andreas Zuber <zuber@puzzle.ch>
2015-08-31 16:31:24 +0200

    implement ip parsing for interface hash

--- lib/dop_common/interface.rb
+++ lib/dop_common/interface.rb
@@ -1,6 +1,7 @@
 #
 # DOP Common configuration lookup
 #
+require 'ipaddr'
 
 module DopCommon
   class Interface
@@ -9,7 +10,28 @@ module DopCommon
 
     def initialize(name, hash)
       @name = name
-      @hash = hash
+      @hash = Hash[hash.map{|k,v| [k.to_sym, v]}]
+    end
+
+    def validate
+      log_validation_method('ip_valid?')
+    end
+
+    def ip
+      @ip ||= ip_valid? ? @hash[:ip] : nil
+    end
+
+  private
+
+    def ip_valid?
+      @hash[:ip].kind_of?(String) or @hash[:ip].kind_of?(Symbol) or
+        raise PlanParsingError, "Interface #{@name}: 'ip' has to be specified as a valid IP String or :dhcp"
+      case @hash[:ip]
+      when :dhcp, 'dhcp' then @hash[:ip] = :dhcp
+      else IPAddr.new(@hash[:ip])
+      end
+    rescue ArgumentError => e
+        raise PlanParsingError, "Interface #{@name}: the specified ip is not valid"
     end
 
   end
---
+++ spec/dop_common/interface_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe DopCommon::Node do
+
+  before :all do
+    DopCommon.log.level = ::Logger::ERROR
+  end
+
+  describe '#ip' do
+    it 'will return :dhcp if dhcp is specified' do
+      interface = DopCommon::Interface.new('eth0', {:ip => 'dhcp'})
+      expect(interface.ip).to eq(:dhcp)
+    end
+    it 'will return the ip if a correct ip is specified' do
+      interface = DopCommon::Interface.new('eth0', {:ip => '192.168.0.1'})
+      expect(interface.ip).to eq('192.168.0.1')
+    end
+    it 'will raise an error if the ip is not valid' do
+      interface = DopCommon::Interface.new('eth0', {:ip => 'not valid ip'})
+      expect{interface.ip}.to raise_error DopCommon::PlanParsingError
+      interface = DopCommon::Interface.new('eth0', {:ip => 2})
+      expect{interface.ip}.to raise_error DopCommon::PlanParsingError
+      interface = DopCommon::Interface.new('eth0', {:ip => '300.0.0.0'})
+      expect{interface.ip}.to raise_error DopCommon::PlanParsingError
+    end
+  end
+
+end
+
