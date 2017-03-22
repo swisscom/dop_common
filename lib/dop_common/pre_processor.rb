a15f21a8174f5f91259e9b752100bf011d4895f4
Andreas Zuber <zuber@puzzle.ch>
2017-03-22 16:35:59 +0100

    implement loop detection in include preprocessor

--- lib/dop_common/pre_processor.rb
+++ lib/dop_common/pre_processor.rb
@@ -11,22 +11,26 @@ module DopCommon
     REGEXP = /(?:^| )include: (\S+)/
 
     def self.load_plan(file)
-      parse_file(file).join
+      file_abs = Pathname.new(file).expand_path.to_s
+      parse_file(file_abs, []).join
     end
 
   private
 
-    def self.parse_file(file)
+    def self.parse_file(file, file_stack)
+      detect_loop(file_stack, file)
       validate_file(file)
       content = []
-      File.readlines(file).each do |line|
+      File.readlines(file).each_with_index do |line, i|
+        new_file_stack = file_stack.dup
+        new_file_stack << [file, i]
         position = (line =~ REGEXP)
         if position
           position += 1 if position > 0
           filtered_name = filter_name(line[REGEXP, 1])
           new_file = absolute_filepath(file, filtered_name)
           spacer = ' ' * position
-          content += parse_file(new_file).map {|l| spacer + l}
+          content += parse_file(new_file, new_file_stack).map {|l| spacer + l}
         else
           content << line
         end
@@ -54,6 +58,15 @@ module DopCommon
         raise PlanParsingError, "PreProcessor: The included file #{file} does not exist!"
       File.readable?(file) or
         raise PlanParsingError, "PreProcessor: The included file #{file} is not readable!"
+    end
+
+    def self.detect_loop(file_stack, file)
+      files = file_stack.map{|f| f[0] }
+      if files.include?(file)
+        files_i = file_stack.map{|f| "#{f[0]}:#{f[1]}" }
+        files_i << file
+        raise PlanParsingError, "PreProcessor: Include loop detected [ #{files_i.join(' --> ')} ]"
+      end
     end
 
   end
